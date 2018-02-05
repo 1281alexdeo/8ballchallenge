b1ddec9ed31cdf3c00c2787d2764a3ddce1d5dbd
skyForge <1281alexdeo@gmail.com>
2018-02-06 04:54:42 +1200

    updating 8ball on screen working

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonAsk = (Button)findViewById(R.id.btnAsk);
+        final ImageView ball8 = (ImageView)findViewById(R.id.image8ball);
+
+        //array to hold all 5 balls
+        final int[] ballArray = {
+                R.drawable.ball12x,
+                R.drawable.ball22x,
+                R.drawable.ball32x,
+                R.drawable.ball42x,
+                R.drawable.ball52x
+
+        };
+
+        //setting action listener to buttonAsk
+        buttonAsk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomNumberGenerator = new Random();
+                int randomNumber = randomNumberGenerator.nextInt(5);
+                Log.d("AskButton","ask button pressed!!" + randomNumber);
+
+                //updating the ball on screen when ask button is pressed
+               ball8.setImageResource(ballArray[randomNumber]);
+            }
+        });
+
+
+
     }
 }
