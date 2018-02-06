71cf30571ac363ac62cbd981b455bd3e091036a6
skyForge <1281alexdeo@gmail.com>
2018-02-06 13:58:00 +1200

    implementing sound when ask button is clicked

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,5 +1,7 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +12,18 @@ import android.widget.ImageView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SoundPool mSoundPool;
+    private int mCSoundId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //using soundpool to play sound on the device
+        //create soundpool
+        mSoundPool = new SoundPool(1,AudioManager.STREAM_MUSIC,0);
+        //load soundID and returning it back
+        mCSoundId =  mSoundPool.load(getApplicationContext(),R.raw.note1_c,1);
         Button buttonAsk = (Button)findViewById(R.id.btnAsk);
 
 
@@ -38,13 +46,17 @@ public class MainActivity extends AppCompatActivity {
                 Random randomNumberGenerator = new Random();
                 int randomNumber = randomNumberGenerator.nextInt(5);
                 Log.d("AskButton","ask button pressed!!" + randomNumber);
-
+                Log.d("AskButton","ask button sound ");
+                mSoundPool.play(mCSoundId,1.0f,1.0f,1,0,1.0f);
                 //updating the ball on screen when ask button is pressed
                ball8.setImageResource(ballArray[randomNumber]);
+
+
             }
         });
 
 
-
     }
+
+
 }
